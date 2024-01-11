7ab8900a16f1124cceb89cb84c3165de6d6279b9
3Dkirill <71841691+3Dkirill@users.noreply.github.com>
2024-01-11 10:57:44 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,257 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "תכנית הבית",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08197752427506089,
+          "pitch": 0.5137910822012444,
+          "rotation": 0,
+          "target": "1-01_-"
+        },
+        {
+          "yaw": 0.3054542298158971,
+          "pitch": 0.38437799038951326,
+          "rotation": 0,
+          "target": "2-02_-"
+        },
+        {
+          "yaw": 0.22485195679355385,
+          "pitch": -0.0432665699312107,
+          "rotation": 0,
+          "target": "3-03_"
+        },
+        {
+          "yaw": -0.1715036811885522,
+          "pitch": -0.028436341469191717,
+          "rotation": 0,
+          "target": "4-04_"
+        },
+        {
+          "yaw": -0.7611437612884533,
+          "pitch": -0.02250548242037098,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01_-",
+      "name": "01_חזית דרום",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.0027041427941654206,
+        "pitch": 0.048688615774683086,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.144632255182021,
+          "pitch": 0.23720661197395465,
+          "rotation": 0,
+          "target": "2-02_-"
+        },
+        {
+          "yaw": -0.0422821175997079,
+          "pitch": -0.5784240284406579,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-02_-",
+      "name": "02_דרום מזרח",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.045647072744465333,
+        "pitch": 0.13129841909378825,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1549704877131255,
+          "pitch": -0.48280700854152414,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 0.5021915746381964,
+          "pitch": 0.1217836095862399,
+          "rotation": 0,
+          "target": "3-03_"
+        },
+        {
+          "yaw": -0.9799840716708186,
+          "pitch": 0.2624983489794648,
+          "rotation": 0,
+          "target": "1-01_-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-03_",
+      "name": "03_גינה",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.3020762166913098,
+        "pitch": 0.048087086491776176,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03561378952135108,
+          "pitch": -0.545382186290535,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -1.5338502161830743,
+          "pitch": 0.20886397726800787,
+          "rotation": 0,
+          "target": "2-02_-"
+        },
+        {
+          "yaw": 0.1302850304285066,
+          "pitch": -0.08456147772426625,
+          "rotation": 0,
+          "target": "4-04_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-04_",
+      "name": "04_סלון",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.002713427956326342,
+        "pitch": 0.21555123786294317,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6057980560673304,
+          "pitch": 0.1989740634403958,
+          "rotation": 0,
+          "target": "3-03_"
+        },
+        {
+          "yaw": -0.020500368590180784,
+          "pitch": -0.5612448166166608,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
